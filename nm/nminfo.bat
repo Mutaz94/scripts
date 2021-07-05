de567c2d3dc0b1854694e1d17d577b38d0601bbd
Mutaz <mutazjab94@gmail.com>
2021-07-04 22:47:05 -0500

    fix directory

--- nm/nminfo.bat
+++ nm/nminfo.bat
@@ -2,8 +2,15 @@
 
 rem This program will facilate nmhelp functionality in Windows OS without the need to move to nm75g65/nmhelp folder
 rem This should be move DIR with PATH access
+echo .
+echo ---------------------------------
+echo  NONMEM help guide
+echo ---------------------------------
+echo . 
 
-set cdir = %cd%
+
+set cdir=%cd%
+cd c:\nm75g64\help
 echo on 
 
 nmhelp.exe %*
