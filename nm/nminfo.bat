9de95fdc9b4382dc99ef2278fc656d67a4599656
Mutaz M. Jaber <jaber038@umn.edu>
2021-07-04 22:51:44 -0500

    Update nminfo.bat

--- nm/nminfo.bat
+++ nm/nminfo.bat
@@ -2,9 +2,12 @@
 
 rem This program will facilate nmhelp functionality in Windows OS without the need to move to nm75g65/nmhelp folder
 rem This should be move DIR with PATH access
+rem Usage: $ nminfo keyword 
+rem keyword can be anything e.g., ADVAN or SIML
+
 
 echo ---------------------------------
-echo  NONMEM help guide
+echo  NONMEM 7.5 help guide
 echo ---------------------------------
 
 
